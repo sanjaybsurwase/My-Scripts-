83b7c96b1fb3f29537b64a78d1154510249e8289
sanjay surwase <sanjaybsurwase@gmail.com>
2024-01-20 19:29:00 +0530

    Echo .sh

--- Echo .sh
+++ Echo .sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-echo "hello sanjay:"
+echo "
+###################script is starting#################################
+Purpost:- to collect evidence
+#######################################################################
+"
+echo hello sanjay: # sanjay surwase
 echo -e "\033[0;31m failed message"
 echo -e "\033[0;32m success message"
 echo -e "\033[0;33m warning message"
 echo -e "\033[0m"
+echo  " This
+is 
+sanjay
+surwase"
